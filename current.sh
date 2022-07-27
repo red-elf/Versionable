a4fdd3c4e4ae497fe9ee5fb2c35920081b57202b
Milos Vasic <milos85vasic@gmail.com>
2022-07-27 21:50:51 +0300

    Print the current version.

--- current.sh
+++ current.sh
@@ -1,1 +1,20 @@
 #!/bin/bash
+
+VERSION_SCRIPT="Version/version.sh"
+
+if test -e "$VERSION_SCRIPT"; then
+
+  # shellcheck disable=SC1090
+  . "$VERSION_SCRIPT"
+  printf "%s.%s" "$VERSIONABLE_VERSION_PRIMARY" "$VERSIONABLE_VERSION_SECONDARY"
+
+  if [ -n "$VERSIONABLE_VERSION_PATCH" ]; then
+
+    printf ".%s" "$VERSIONABLE_VERSION_PATCH"
+  fi
+
+  printf "\n"
+else
+
+  echo "ERROR: The '$VERSION_VERSION_SCRIPT' script not found"
+fi
