34d52c863187e44914968057d3222d9d53e0a1d7
Milos Vasic <milos85vasic@gmail.com>
2022-07-28 18:26:36 +0300

    Extending scripts.

--- installed.sh
+++ installed.sh
@@ -1,2 +1,22 @@
 #!/bin/bash
 
+VERSION_SCRIPT="Version/version.sh"
+
+if test -e "$VERSION_SCRIPT"; then
+
+  # shellcheck disable=SC1090
+  . "$VERSION_SCRIPT"
+
+  VERSION_EXECUTABLE=/usr/local/bin/"${VERSIONABLE_NAME}"_Version/"${VERSIONABLE_NAME}"_Version
+
+  if test -e "$VERSION_EXECUTABLE"; then
+
+    "$VERSION_EXECUTABLE"
+  else
+
+    echo "No version information available for the '${VERSIONABLE_NAME}'"
+  fi
+else
+
+  echo "ERROR: The '$VERSION_VERSION_SCRIPT' script not found"
+fi
