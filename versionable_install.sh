634e0ca4e968fcbda17afaaef2bdbdfd7e21ce7a
Milos Vasic <milos85vasic@gmail.com>
2022-07-31 20:20:08 +0300

    Extract to the build and install scripts.

--- versionable_install.sh
+++ versionable_install.sh
@@ -5,9 +5,19 @@ if [ -z "$1" ]; then
   exit 1
 fi
 
+HERE="$(pwd)"
 TARGET="$1"
+VERSION_SCRIPT="$HERE/Version/version.sh"
 
-echo "Please provide your SUDO password in order to install the '$TARGET' target to your system" && \
-cd "$TARGET/Build" &&
-sudo ninja install && \
-echo "The '$TARGET' target has been installed with success"
+if ! test -e "$VERSION_SCRIPT"; then
+
+  echo "ERROR: The version script was not found at path: '$VERSION_SCRIPT'"
+  exit 1
+fi
+
+# shellcheck disable=SC1090
+echo "Please provide your SUDO password in order to install the '$VERSIONABLE_NAME' target to your system" && \
+  source "$VERSION_SCRIPT" && \
+  cd "$TARGET/Build" && \
+  sudo ninja install && \
+  echo "The '$VERSIONABLE_NAME' target has been installed with success"
