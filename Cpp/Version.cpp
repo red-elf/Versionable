7d495e9956bcd0b3c4970989bbfae395ffd594cc
Milos Vasic <milos85vasic@gmail.com>
2022-08-01 19:57:05 +0300

    Using the SNAPSHOT flag.

--- Cpp/Version.cpp
+++ Cpp/Version.cpp
@@ -6,7 +6,14 @@
 int main() {
 
     std::cout << std::to_string(VERSIONABLE_VERSION_PRIMARY) <<
-        "." << std::to_string(VERSIONABLE_VERSION_SECONDARY) <<
-        "." << std::to_string(VERSIONABLE_VERSION_PATCH) << std::endl;
+              "." << std::to_string(VERSIONABLE_VERSION_SECONDARY) <<
+              "." << std::to_string(VERSIONABLE_VERSION_PATCH);
+
+    if (VERSIONABLE_SNAPSHOT) {
+
+        std::cout << "-SNAPSHOT";
+    }
+
+    std::cout << std::endl;
     return 0;
 }
