8b49d2acb97168ff07aff1fe899bb4d9652e0bc1
Milos Vasic <milos85vasic@gmail.com>
2022-08-01 19:53:54 +0300

    Using the SNAPSHOT flag.

--- versionable_build.sh
+++ versionable_build.sh
@@ -24,13 +24,15 @@ if ! test -e "$VERSION_SCRIPT"; then
 fi
 
 # shellcheck disable=SC1090
-echo "Installing the '$VERSIONABLE_NAME' target, please wait" &&
+source "$VERSION_SCRIPT" && \
+  echo "Installing the '$VERSIONABLE_NAME' target, please wait" &&
   cd "$TARGET" &&
   rm -rf ./Build &&
   mkdir Build &&
   cd Build &&
   source "$VERSION_SCRIPT" && cmake -GNinja -DVERSIONABLE_VERSION_PRIMARY="$VERSIONABLE_VERSION_PRIMARY" \
   -DVERSIONABLE_VERSION_SECONDARY="$VERSIONABLE_VERSION_SECONDARY" -DVERSIONABLE_NAME="$VERSIONABLE_NAME" \
-  -DVERSIONABLE_VERSION_PATCH="$VERSIONABLE_VERSION_PATCH" "$CMAKE_PATH" && \
+  -DVERSIONABLE_VERSION_PATCH="$VERSIONABLE_VERSION_PATCH" -DVERSIONABLE_SNAPSHOT="$VERSIONABLE_SNAPSHOT" \
+  "$CMAKE_PATH" && \
   ninja -j "$(nproc)" && \
   echo "The '$VERSIONABLE_NAME' target has been compiled with success"
