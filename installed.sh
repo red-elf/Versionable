b5c5d7c180bca9b2c73d163936b3acf46e613645
Milos Vasic <milos85vasic@gmail.com>
2022-08-12 20:18:43 +0300

    Versionable: The search path extended to the binary items.

--- installed.sh
+++ installed.sh
@@ -1,34 +1,46 @@
 #!/bin/bash
 
-SEARCH_PATH="/usr/local/lib"
-VERSION_SCRIPT="Version/version.sh"
-INSTALLED_SCRIPT="Version/installed.sh"
+function GET_INSTALLED_VERSION {
 
-if test -e "$INSTALLED_SCRIPT"; then
+  SEARCH_PATH="/usr/local/lib"
 
-  sh "$INSTALLED_SCRIPT"
+  if [ -n "$1" ]; then
 
-else
-
-  if test -e "$VERSION_SCRIPT"; then
-
-    # shellcheck disable=SC1090
-    . "$VERSION_SCRIPT"
-
-    if [ -z "$VERSIONABLE_NAME" ]; then
-
-      echo "ERROR: The version name not set"
-      exit 1
-    fi
-
-    for entry in "$SEARCH_PATH"/*; do
-
-      FULL_ENTRY="$(echo "$entry" | grep "$VERSIONABLE_NAME")"
-
-      if ! [[ "$FULL_ENTRY" = "" ]]; then
-
-        echo "${FULL_ENTRY/"$SEARCH_PATH/$VERSIONABLE_NAME-"/}"
-      fi
-    done
+    SEARCH_PATH="$1"
   fi
-fi
+
+  VERSION_SCRIPT="Version/version.sh"
+  INSTALLED_SCRIPT="Version/installed.sh"
+
+  if test -e "$INSTALLED_SCRIPT"; then
+
+    sh "$INSTALLED_SCRIPT"
+
+  else
+
+    if test -e "$VERSION_SCRIPT"; then
+
+      # shellcheck disable=SC1090
+      . "$VERSION_SCRIPT"
+
+      if [ -z "$VERSIONABLE_NAME" ]; then
+
+        echo "ERROR: The version name not set"
+        exit 1
+      fi
+
+      for entry in "$SEARCH_PATH"/*; do
+
+        FULL_ENTRY="$(echo "$entry" | grep "$VERSIONABLE_NAME")"
+
+        if ! [[ "$FULL_ENTRY" = "" ]]; then
+
+          echo "${FULL_ENTRY/"$SEARCH_PATH/$VERSIONABLE_NAME-"/}"
+        fi
+      done
+    fi
+  fi
+}
+
+GET_INSTALLED_VERSION "/usr/local/lib"
+GET_INSTALLED_VERSION "/usr/local/bin"
